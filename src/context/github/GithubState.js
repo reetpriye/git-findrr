154ddf3d4fe009f4b4c7cf790bad14776a15c6fb
Reet Priye <reetpriye@gmail.com>
2020-12-13 11:31:10 +0530

    .env update

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -10,6 +10,18 @@ import {
   GET_REPOS
 } from '../types';
 
+let githubClientId;
+let githubClientSecret;
+
+if(process.env.NODE_ENV !== 'production') {
+  githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+}
+ else {
+  githubClientId = process.env.GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+ }
+
 const GithubState = props => {
   const initialState = {
     users: [],
@@ -24,7 +36,7 @@ const GithubState = props => {
   const searchUsers = async text => {
 
     // API Call URL with search functionality
-    const url = `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`;
+    const url = `https://api.github.com/search/users?q=${text}&client_id=${githubClientId}&client_secret=${githubClientSecret}`;
 
     setLoading();
 
@@ -38,7 +50,7 @@ const GithubState = props => {
   // Get User
   const getUser = async username => {
     // API Call URL with search functionality
-    const url = `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`;
+    const url = `https://api.github.com/users/${username}?client_id=${githubClientId}&client_secret=${githubClientSecret}`;
 
     setLoading();
     
@@ -50,7 +62,7 @@ const GithubState = props => {
   // Get Repos
   const getRepos = async username => {
     // API Call URL with search functionality
-    const url = `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`;
+    const url = `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${githubClientId}&client_secret=${githubClientSecret}`;
 
     setLoading();
 
